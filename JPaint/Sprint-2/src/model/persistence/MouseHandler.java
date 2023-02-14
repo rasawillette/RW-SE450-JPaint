d850c985c0bd74e210e4d26ff506b07209759def
rasawillette <rasawillette@gmail.com>
2023-02-13 17:12:24 -0700

    Add files via upload

---
+++ JPaint/Sprint-2/src/model/persistence/MouseHandler.java
@@ -0,0 +1,85 @@
+package model.persistence;
+
+import model.MouseMode;
+import model.interfaces.IMouseState;
+import model.shapes.Point;
+import model.shapes.ShapeList;
+import view.gui.PaintCanvas;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+// override 2 methods
+public class MouseHandler implements MouseListener {
+
+    public Point startPoint;
+    public Point endPoint;
+
+    //IGuiWindow guiWindow;
+    ApplicationState applicationState;
+    PaintCanvas paintCanvas;
+    ShapeList shapeList;
+    IMouseState mouseState;
+
+    public MouseHandler(ApplicationState applicationState,PaintCanvas paintCanvas,ShapeList shapeList){
+        this.applicationState = applicationState;
+        this.paintCanvas = paintCanvas;
+        this.shapeList = shapeList;
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {}
+
+    // mouse pressed
+    @Override
+    public void mousePressed(MouseEvent e) {
+        startPoint = new Point();
+        startPoint.x = e.getX();
+        startPoint.y = e.getY();
+        applicationState.setStart(startPoint);
+        System.out.println("mouse pressed" + startPoint.x + " " + startPoint.y);
+    }
+
+    // mouse released
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        endPoint = new Point();
+        endPoint.x = e.getX();
+        endPoint.y = e.getY();
+        applicationState.setStop(endPoint);
+
+        // calculate height
+        int height = (int) Math.abs(endPoint.y- startPoint.y);
+        // calculate width
+        int width = (int) Math.abs(endPoint.x- startPoint.x);
+
+        System.out.println("mouse released" + height + " " + width);
+        e.getComponent().repaint();
+
+        if (applicationState.getActiveMouseMode().equals(MouseMode.DRAW)) {
+            mouseState = new DrawMouseState();
+            mouseState.execute(startPoint,endPoint,applicationState,paintCanvas,shapeList);
+        }
+
+        else if (applicationState.getActiveMouseMode().equals(MouseMode.SELECT)) {
+            mouseState = new SelectMouseState();
+            mouseState.execute(startPoint,endPoint,applicationState,paintCanvas,shapeList);
+        }
+
+        else if(applicationState.getActiveMouseMode().equals(MouseMode.MOVE)){
+            //mouseState = new MoveMouseState();
+            //mouseState.execute(startPoint,endPoint,applicationState,paintCanvas,shapeList);
+        }
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {}
+
+    @Override
+    public void mouseExited(MouseEvent e) {}
+
+    public void addState(ApplicationState applicationState) {
+        this.applicationState = applicationState;
+    }
+
+}
