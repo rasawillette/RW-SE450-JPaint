1092f88c53f11b59c7af26c085550ec4b16a83fb
rasawillette <rasawillette@gmail.com>
2023-02-13 17:05:28 -0700

    Add files via upload

---
+++ JPaint/Sprint-2/src/controller/Component.java
@@ -0,0 +1,19 @@
+package controller;
+
+import model.shapes.ShapeColor;
+import model.shapes.ShapeShadingType;
+import model.shapes.ShapeType;
+
+public class Component {
+
+    //static public IShape;
+    static public ShapeType activeShapeType;
+    static public ShapeColor activePrimaryColor;
+    static public ShapeColor activeSecondaryColor;
+    static public ShapeShadingType activeShapeShadingType;
+
+    static public int startX;
+    static public int startY;
+    static public int height;
+    static public int width;
+}
